19a52a29ed7cf1e85211cd090fe7d722848ce135
Roman <VXlch_vg85@student.itstep.org>
2025-06-18 16:01:53 +0300

    ht_storage

--- 11_Storage_task_and_ht/js/task.js
+++ 11_Storage_task_and_ht/js/task.js
@@ -5,24 +5,38 @@ function getRandomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-
     return `rgb(${r}, ${g}, ${b})`;
 }
-function createBlock() {
+
+function createBlock(color) {
     let block = document.createElement('div');
     block.classList.add('block');
-    block.style.backgroundColor = getRandomColor();
+    block.style.backgroundColor = color;
     return block;
 }
 
+function saveBlocks() {
+    let colors = [];
+    document.querySelectorAll('.block').forEach(block => {
+        colors.push(block.style.backgroundColor);
+    });
+    localStorage.setItem('blocks', JSON.stringify(colors));
+}
+
+function loadBlocks() {
+    let colors = JSON.parse(localStorage.getItem('blocks') || '[]');
+    colors.forEach(color => {
+        let block = createBlock(color);
+        blockContainer.appendChild(block);
+    });
+}
+
 btn.addEventListener('click', () => {
-    let newBlock = createBlock();
+    let color = getRandomColor();
+    let newBlock = createBlock(color);
     blockContainer.appendChild(newBlock);
+    saveBlocks();
 });
-
-
-
-
 
 
 let btn2 = document.getElementById("btn")
@@ -30,21 +44,25 @@ let circle1 = document.getElementById("1")
 let circle2 = document.getElementById("2")
 let circle3 = document.getElementById("3")
 
-let id = 0;
-btn2.onclick = function () {
+let id = +localStorage.getItem('lightState') || 0;
+
+function updateTrafficLight(state) {
     circle1.style.backgroundColor = "rgb(157, 157, 157)";
     circle2.style.backgroundColor = "rgb(157, 157, 157)";
     circle3.style.backgroundColor = "rgb(157, 157, 157)";
-    if (id == 0) {
-        id++;
-        circle1.style.backgroundColor = "red"
-    }
-    else if (id == 1) {
-        id++;
-        circle2.style.backgroundColor = "yellow"
-    }
-    else if (id == 2) {
-        id = 0;
-        circle3.style.backgroundColor = "green"
-    }
+    
+    if (state === 0) circle1.style.backgroundColor = "red";
+    else if (state === 1) circle2.style.backgroundColor = "yellow";
+    else if (state === 2) circle3.style.backgroundColor = "green";
 }
+
+btn2.onclick = function () {
+    id = (id + 1) % 3;
+    updateTrafficLight(id);
+    localStorage.setItem('lightState', id);
+};
+
+window.onload = () => {
+    loadBlocks();
+    updateTrafficLight(id);
+};
